696ce8fbab2ca847da2f3a371b0efd51d9753661
nebert <nico.ebert@gmail.com>
2021-09-04 16:27:59 +0200

    Update qbutterfly.js

--- qbutterfly.js
+++ qbutterfly.js
@@ -35,6 +35,7 @@ var qualtricsURL;
           currentTime: 	Date.now(),
         },
         qualtricsURL); 
+        log("Message sent: OnLoad);  
     }
     window.onpageshow = function(evt) { if (evt.persisted) disableBack() }
 });
